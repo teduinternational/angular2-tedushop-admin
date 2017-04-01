1247db578af07962f0aeead1c296b0a0dfecfdb2
Toan Bach Ngoc <toan.bach-ngoc@tek-experts.com>
2017-04-01 10:45:15 +0700

    Initial commmit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ProductComponent } from './components/product/product.component';
+import { ProductCategoryComponent } from './components/product-category/product-category.component';
+import { HomeComponent } from './components/home/home.component';
+import { SidebarMenuComponent } from './shared/sidebar-menu/sidebar-menu.component';
+import { TopMenuComponent } from './shared/top-menu/top-menu.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'product', component: ProductComponent },
+  { path: 'product-category', component: ProductCategoryComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    ProductCategoryComponent,
+    HomeComponent,
+    SidebarMenuComponent,
+    TopMenuComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
