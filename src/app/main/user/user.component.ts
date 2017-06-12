406c5f958e38b2830092e8a042bfb83629b52f0e
Toanbn <ngoctoan89@gmail.com>
2017-06-13 06:52:43 +0700

    format date and reset form

--- src/app/main/user/user.component.ts
+++ src/app/main/user/user.component.ts
@@ -11,6 +11,7 @@ import { UploadService } from '../../core/services/upload.service';
 import { ModalDirective } from 'ngx-bootstrap/modal';
 import { IMultiSelectOption } from 'angular-2-dropdown-multiselect';
 import { Router } from '@angular/router';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-user',
@@ -90,27 +91,28 @@ export class UserComponent implements OnInit {
     }, error => this._dataService.handleError(error));
   }
   //Save change for modal popup
-  public saveChanges(valid: boolean) {
-    if (valid) {
+  public saveChanges(form: NgForm) {
+    if (form.valid) {
       this.entity.Roles = this.myRoles;
       let fi = this.avatar.nativeElement;
       if (fi.files.length > 0) {
         this.uploadService.postWithFile('/api/upload/saveImage?type=avatar', null, fi.files).then((imageUrl: string) => {
           this.entity.Avatar = imageUrl;
         }).then(() => {
-          this.saveData();
+          this.saveData(form);
         });
       }
       else {
-        this.saveData();
+        this.saveData(form);
       }
     }
   }
-  private saveData() {
+  private saveData(form: NgForm) {
     if (this.entity.Id == undefined) {
       this._dataService.post('/api/appUser/add', JSON.stringify(this.entity)).subscribe((response: any) => {
         this.search();
         this.addEditModal.hide();
+        form.resetForm();
         this.notificationService.printSuccessMessage(MessageContstants.CREATED_OK_MSG);
       });
     }
@@ -118,6 +120,7 @@ export class UserComponent implements OnInit {
       this._dataService.put('/api/appUser/update', JSON.stringify(this.entity)).subscribe((response: any) => {
         this.search();
         this.addEditModal.hide();
+         form.resetForm();
         this.notificationService.printSuccessMessage(MessageContstants.UPDATED_OK_MSG);
       }, error => this._dataService.handleError(error));
     }
@@ -131,7 +134,8 @@ export class UserComponent implements OnInit {
   }
 
   public selectedDate(value: any) {
-    console.log(value);
+   
     this.entity.BirthDay = moment(new Date(value.end._d)).format('DD/MM/YYYY');
+     console.log(this.entity.BirthDay);
   }
 }
